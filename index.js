452c4207296fe4bd60dd9724994eb2793f0e017f
Akib Saleh <akibzihan@gmail.com>
2023-11-11 23:55:31 +0600

    Search Api Developed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const multer = require('multer');
-const uuid = require('uuid').v4;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -12,25 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    const { originalname } = file;
-    cb(null, `${uuid()}-${originalname}`);
-  },
-});
-=======
-const upload = multer()
->>>>>>> 6813c38cd9b3b042cfee3edacc65884b48275c48
+const upload = multer();
 
-const upload = multer({ storage });
-
-const multiUpload = upload.fields([
-  { name: 'coverPhoto', maxCount: 1 },
-  { name: 'gallery', maxCount: 3 },
+const fileUpload = upload.fields([
+  { name: 'mainFile', maxCount: 1 },
+  { name: 'others', maxCount: 3 },
 ]);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.uviemuc.mongodb.net/applicationsDb?retryWrites=true&w=majority`;
@@ -47,15 +32,27 @@ async function run() {
   try {
     const applicationsCollection = client.db('applicationsDb').collection('applications');
 
-    app.post('/submission', multiUpload, async (req, res) => {
-      res.send({ status: 'success' });
-    });
-
     app.get('/applications', async (req, res) => {
       const result = await applicationsCollection.find().toArray();
       res.send(result);
     });
 
+    app.get('/checkApplicants', async (req, res) => {
+      const urlQuery = req.query?.phone;
+      const query = { phone: urlQuery };
+      const result = await applicationsCollection.findOne(query);
+      if (result) res.send('present');
+      else res.send('absent');
+    });
+
+    app.get('/searchResult', async (req, res) => {
+      const urlQuery = req.query?.search;
+      const query = { name: { $regex: `.*${urlQuery}.*`, $options: 'i' } };
+      const result = await applicationsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/applications/count', async (req, res) => {});
 
     app.get('/applications/:id', async (req, res) => {
       const id = req.params.id;
@@ -82,40 +79,16 @@ async function run() {
       res.send(result);
     });
 
-<<<<<<< HEAD
-    // app.post('/applications', fileUpload, async (req, res) => {
-    //   const applications = req.body;
-    //   const serverAddress = `${req.protocol}://${req.get('host')}/`;
-    //   const image = serverAddress + req.files['mainFile'][0].path.replace(/\\/g, '/');
-    //   console.log(req.files['mainFile'][0]);
-    //   const images = req.files['others'];
-    //   const imagesPath = images?.map((image) => serverAddress + image.path.replace(/\\/g, '/'));
-
-    //   try {
-    //     const result = await applicationsCollection.insertOne({
-    //       ...applications,
-    //       image: image,
-    //       others: imagesPath,
-    //       createdAt: new Date(),
-    //     });
-    //     res.send(result);
-    //   } catch (error) {
-    //     console.log(error);
-    //     res.status(400).send(error.message);
-    //   }
-    // });
-=======
     app.post('/applications', fileUpload, async (req, res) => {
-      const applications = req.body;  
+      const applications = req.body;
       try {
         const result = await applicationsCollection.insertOne(applications);
         res.send(result);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(400).send(error.message);
       }
     });
->>>>>>> 6813c38cd9b3b042cfee3edacc65884b48275c48
 
     app.put('/applications/:id', async (req, res) => {
       const id = req.params.id;
@@ -133,7 +106,7 @@ async function run() {
       res.send({ result, latestRequest });
     });
 
-    await client.connect();
+    // await client.connect();
     await client.db('admin').command({ ping: 1 });
     console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
